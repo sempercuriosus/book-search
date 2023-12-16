55ecf343c53f11f84ffa02bc0a4f786f51041b29
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-16 14:59:11 -0500

    working on setting up the typeDefs, resolvers, and how to work from the models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,24 @@
+const { User, Book } = required('../models');
+
 console.info('--- INFORMATION --->', 'resolvers loaded');
+
+const resolvers = {
+
+    // QUERY START
+    Query: {
+        users_all: async () => {
+            return User.find({});
+        },
+        book_all: async () => {
+            return Book.find({});
+        }
+    },
+
+    // MUTATION START
+    Mutation: {
+
+    }
+
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,44 @@
 console.info('--- INFORMATION --->', 'type defs loaded');
+
+/* 
+ * TYPE DEFINITIONS (Schema)
+ * ---
+ * Define the structure and capabilities of your GraphQL API.
+ * types are objects, object type specifically
+ * they are more flexible and are have no direct link to a table
+ * --- They are a BLUEPRINT ---
+ * they are a middleman between client and server SPECIFICALLY for the structure and interactions for the data
+ * 
+ * typeDefs specify the types available
+ * - their relationships between types
+ * - queries
+ * - mutations
+ * 
+ * Think of typeDefs as a blueprint for your GraphQL API, providing a clear contract between the client and server regarding the expected structure of data.
+ * MUTATIONS
+ * ---
+ * Define interations one may take with the data
+ * follow CRUD
+ * not only DATABASE actions but also SERVER actions
+*/
+
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+}
+
+type Book {
+    _id: ID!
+    author: String!
+    title: String!
+    link: String
+}
+
+mutation BookCount{
+
+}
+`;
+
+module.exports = typeDefs;
