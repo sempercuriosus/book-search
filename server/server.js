d06ec65e2e87555dfcb2f41e5b22c7a1c4d83cdd
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-16 16:30:18 -0500

    added apollo dependency and also outline of implementation

--- server/server.js
+++ server/server.js
@@ -1,21 +1,74 @@
+
+/*
+  * IMPLEMENTING APOLLO SERVER
+  * 
+  * server
+  * - import apollo server 
+  * - setup express middleware
+  * 
+  * - discontinue routes router
+  * 
+  * database
+  * - import schema
+  * - - type defs and resolvers
+  * 
+  * connection 
+  * - add connection information
+  * 
+  * server
+  * - declare the server
+  * - express use declaration
+  * - setup apollo server and make sure it is asynchronous
+  * - - await the server start
+  * - - middleware declarations
+  * - - - (include graph ql)
+  * - - db open, start app listening on PORT
+  * 
+  * start apollo server
+*/
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// discontinued
+// const routes = require('./routes');
 
-const app = express();
+
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { typeDefs, resolvers } = require('./schema');
+
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs
+  , resolvers
 });
+
+const app = express();
+
+const startApolloServer = async () => {
+
+  await server.start();
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  // in updating the code base, this has been discontinued
+  // app.use(routes);
+
+  // app will now use graphql as the router
+  // setting up an endpoint and passing the server, server, as an argument
+  app.use('/graphql', expressMiddleware(server));
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+
+};
+
+startApolloServer();
