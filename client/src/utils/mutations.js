1eaed65f0867298fb90bfc96d3f17c60a52b4de2
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-17 18:35:02 -0500

    moved login and added delete placeholder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,23 +3,23 @@ import { gql } from '@apollo/client';
 
 // EXPORT MUTATIONS
 
+export const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        _id
+      }
+      token
+    }
+  }
+`;
+
 export const ADD_USER = gql`
   mutation AddUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       user {
         username
         email
-      }
-    }
-  }
-`;
-
-export const LOGIN_USER = gql`
-  mutation Login($password: String!, $username: String) {
-    login(password: $password, username: $username) {
-      token
-      user {
-        username
       }
     }
   }
@@ -34,5 +34,11 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const REMOVE_BOOK = gql``;
+export const REMOVE_BOOK = gql`
+  mutation DeleteBook {
+    deleteBook {
+      bookId
+    }
+  }
+`;
 
