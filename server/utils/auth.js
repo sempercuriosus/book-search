0f85ee229b5c2bff24b6aed090bd2c39aec55c8b
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-20 10:35:36 -0500

    did not have a request to server contex set. added new function for this

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,6 +12,31 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
+  authMiddleware: ({ req }) => {
+    // req has a headers section, which has an authorization to it, using that.
+    // console.log(req.headers);
+    let token = req.headers.authorization;
+
+    const spaceIndex = token.indexOf(' ');
+
+    token = token.substring(spaceIndex).trim();
+
+    console.log(token);
+
+    if (!token) {
+      return 'Token Not Set';
+    }
+
+    try {
+      // ASSIGNING THE REQUEST USER CONTEXT FOR SERVER USE
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid Operation...');
+    }
+
+    return req;
+  },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
