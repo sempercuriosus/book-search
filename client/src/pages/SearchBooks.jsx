a86b02e7e20ec4636643fc507d009672cac9f85b
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-20 13:48:11 -0500

    replaced api

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,16 +1,12 @@
 import { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
+import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { canonicalStringify } from '@apollo/client/cache';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,13 +16,28 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
+  //
+  //
+  //
+  //
+  // this needs updating
+  //
+  //
+  //
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
 
+  //
+  //
+  //
+  //
+  // this needs updating
+  //
+  //
+  //
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -36,13 +47,19 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      // rather than using the api itself, use fetch to call the endpoint you are constructing.
+      const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=$';
+      const requestUrl = baseUrl + searchInput;
+      const response = await fetch(requestUrl);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
       const { items } = await response.json();
+      // const booksFound = await response.json();
+
+      console.log(items);
 
       const bookData = items.map((book) => ({
         bookId: book.id,
@@ -53,7 +70,8 @@ const SearchBooks = () => {
       }));
 
       setSearchedBooks(bookData);
-      setSearchInput('');
+      //
+      // setSearchInput('');
     } catch (err) {
       console.error(err);
     }
@@ -72,6 +90,14 @@ const SearchBooks = () => {
     }
 
     try {
+      //
+      //
+      //
+      //
+      // this needs updating
+      //
+      //
+      //
       const response = await saveBook(bookToSave, token);
 
       if (!response.ok) {
@@ -87,12 +113,14 @@ const SearchBooks = () => {
 
   return (
     <>
-      <div className="text-light bg-dark p-5">
+      <div className='text-light bg-dark p-5'>
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
             <Row>
-              <Col xs={12} md={8}>
+              <Col
+                xs={12}
+                md={8}>
                 <Form.Control
                   name='searchInput'
                   value={searchInput}
@@ -102,8 +130,13 @@ const SearchBooks = () => {
                   placeholder='Search for a book'
                 />
               </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
+              <Col
+                xs={12}
+                md={4}>
+                <Button
+                  type='submit'
+                  variant='success'
+                  size='lg'>
                   Submit Search
                 </Button>
               </Col>
@@ -121,10 +154,16 @@ const SearchBooks = () => {
         <Row>
           {searchedBooks.map((book) => {
             return (
-              <Col md="4" key={book.bookId}>
+              <Col
+                md='4'
+                key={book.bookId}>
                 <Card border='dark'>
                   {book.image ? (
-                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant='top'
+                    />
                   ) : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
@@ -132,10 +171,14 @@ const SearchBooks = () => {
                     <Card.Text>{book.description}</Card.Text>
                     {Auth.loggedIn() && (
                       <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                        disabled={savedBookIds?.some(
+                          (savedBookId) => savedBookId === book.bookId,
+                        )}
                         className='btn-block btn-info'
                         onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
+                        {savedBookIds?.some(
+                          (savedBookId) => savedBookId === book.bookId,
+                        )
                           ? 'This book has already been saved!'
                           : 'Save this Book!'}
                       </Button>
@@ -152,3 +195,4 @@ const SearchBooks = () => {
 };
 
 export default SearchBooks;
+
